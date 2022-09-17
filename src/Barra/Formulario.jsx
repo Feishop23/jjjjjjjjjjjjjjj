a6edc3a1bb416f91fc502e7fd7e8f0f83e5242d0
Feishop23 <feiberth10portilla@hotmail.com>
2022-09-17 15:40:06 -0400

    agregacion de texto en formulario

--- src/Barra/Formulario.jsx
+++ src/Barra/Formulario.jsx
@@ -9,6 +9,8 @@ export const Formulario = () => {
         <input onChange={e=>setdataForm(e.target.value)} value={dataForm.nombre} type="text" className="form_input" placeholder="Name:" required name="Nombre"  id="name"/>
         <input  onChange={e=>setdataForm(e.target.value)} value={dataForm.email} type="email" className="form_input" placeholder="Email:" required name="Correo" id="email"/>
         <textarea  onChange={e=>setdataForm(e.target.value)} value={dataForm.mensaje} className="form_input form_input--message" placeholder="Message:" required id="message" name="Mensaje"></textarea>
+        <p className='parrafoFormulario'>Check the box on the form page and verify that you are not a robot for the form to be submitted.
+</p>
         <input type="submit"  target='_blank' value="Send" className="form_cta"/>
         </form>
     </div>
