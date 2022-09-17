e022820fb4065a6d1651763056986d211b6205fc
Feiberth Portilla <100066412+Feishop23@users.noreply.github.com>
2022-09-17 11:37:12 -0500

    Update Formulario.jsx

--- src/Barra/Formulario.jsx
+++ src/Barra/Formulario.jsx
@@ -6,10 +6,10 @@ export const Formulario = () => {
   return (
     <div className='Formulario' >
         <form onSubmit={handleSubmit} action="https://formsubmit.co/feiberth10portilla@hotmail.com"  target='_blank' method="POST" >
-        <input onChange={e=>setdataForm(e.target.value)} value={dataForm.nombre} type="text" className="form_input" placeholder="Nombre:" required name="Nombre"  id="name"/>
+        <input onChange={e=>setdataForm(e.target.value)} value={dataForm.nombre} type="text" className="form_input" placeholder="Name:" required name="Nombre"  id="name"/>
         <input  onChange={e=>setdataForm(e.target.value)} value={dataForm.email} type="email" className="form_input" placeholder="Email:" required name="Correo" id="email"/>
-        <textarea  onChange={e=>setdataForm(e.target.value)} value={dataForm.mensaje} className="form_input form_input--message" placeholder="Mensaje:" required id="message" name="Mensaje"></textarea>
-        <input type="submit"  target='_blank' value="Enviar" className="form_cta"/>
+        <textarea  onChange={e=>setdataForm(e.target.value)} value={dataForm.mensaje} className="form_input form_input--message" placeholder="Message:" required id="message" name="Mensaje"></textarea>
+        <input type="submit"  target='_blank' value="Send" className="form_cta"/>
         </form>
     </div>
   )
